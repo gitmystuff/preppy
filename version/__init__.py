074d770aa6924284ee9ba058d6a23a274e5edf6f
gitmystuff <cliffo@bluetoe.org>
2024-04-16 16:27:40 +0000

    version edit

--- version/__init__.py
+++ version/__init__.py
@@ -2,4 +2,4 @@ __major_version__ = '0'
 __minor_version__ = '1'
 __patch_version__ = '0'
 
-__version__ = f'DTSC 4050 Version: {__major_version__}.{__minor_version__}.{__patch_version__}'
+__version__ = f'PrepPy Version: {__major_version__}.{__minor_version__}.{__patch_version__}'
