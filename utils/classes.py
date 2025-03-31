ea7d7236d9063d393122712a88bdce963a1f5c4a
gitmystuff <cliffo@bluetoe.org>
2025-03-31 16:00:24 -0500

    Update classes.py

--- utils/classes.py
+++ utils/classes.py
@@ -64,15 +64,32 @@ class HandleMissingValues(BaseEstimator, TransformerMixin):
     return X.values
 
 class PerformOHE(BaseEstimator, TransformerMixin):
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def fit(self, X, y=None):
-    return self
+    def fit(self, X, y=None):
+        return self
 
-  def transform(self, X):
-    if isinstance(X, np.ndarray):
-        X = pd.DataFrame(X)
+    def transform(self, X):
+        if isinstance(X, np.ndarray):
+            X = pd.DataFrame(X)
 
-    X = utils.do_OHE(X)
-    return X.values
+        cat_features = []
+        for feat in X.select_dtypes(include=['object', 'category']):
+            if len(X[feat].value_counts()) < 3:
+                X[feat] = X[feat].map({X[feat].value_counts().index[0]: 0, X[feat].value_counts().index[1]: 1})
+                X[feat] = X[feat].astype(int)
+            elif 2 < len(X[feat].value_counts()) < 6:
+                cat_features.append(feat)
+            elif len(X[feat].value_counts()) > 5:
+                freq = X.groupby(feat, observed=False).size() / len(X)
+                X[feat] = X[feat].map(freq)
+
+        if cat_features:
+            ohe = OneHotEncoder(categories='auto', drop='first', sparse_output=False, handle_unknown='ignore')
+            ohe_array = ohe.fit_transform(X[cat_features])
+            ohe_df = pd.DataFrame(ohe_array, columns=ohe.get_feature_names_out(cat_features), index=X.index)
+            X = X.join(ohe_df)
+            X.drop(cat_features, axis=1, inplace=True)
+          
+        return X
